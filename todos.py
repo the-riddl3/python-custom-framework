a90f5ca065105fbef1895400104a09c443a8f892
vajbon3 <notvaja@gmail.com>
2024-05-12 16:41:32 +0400

    untrack files added to gitignore

--- todos.py
+++
@@ -1,6 +0,0 @@
-from http import json_response, match_response
-
-
-def get_todos(query_params: dict, headers: dict, payload: dict):
-    return match_response(headers, {"message": "Why are you trying to read my todo list bruh"})
-
